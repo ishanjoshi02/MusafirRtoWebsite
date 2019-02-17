eae3d199d2019e9ecf9ab242871a435f6c29b517
Ishan Joshi <ishanjoshi02@gmail.com>
2019-02-17 10:26:48 +0530

    added user state persistence

--- src/Firebase/auth.js
+++ src/Firebase/auth.js
@@ -1,12 +1,13 @@
 import { auth } from "./firebase";
 
 // Sign Up
-export const doCreateUserWithEmailAndPassword = ({ email, password }) =>
-  auth.createUserWithEmailAndPassword(email, password);
-
+export const doCreateUserWithEmailAndPassword = ({ email, password }) => {
+  return auth.createUserWithEmailAndPassword(email, password);
+};
 // Sign In
-export const doSignInWithEmailAndPassword = ({ email, password }) =>
-  auth.signInWithEmailAndPassword(email, password);
+export const doSignInWithEmailAndPassword = ({ email, password }) => {
+  return auth.signInAndRetrieveDataWithEmailAndPassword(email, password);
+};
 
 // Sign Out
 export const doSignOut = () => auth.signOut();
--- src/HOC/isAuth.jsx
+++ src/HOC/isAuth.jsx
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import { auth } from "../Firebase";
 import Loading from "../components/Loading/Loading";
 
+import * as firebase from "firebase";
 export default (ComposedClass, reload) => {
   class AuthenticationCheck extends Component {
     state = { loading: true };
-    componentWillMount = () => {
-      // get current user from firebase
-      const user = auth.getUser();
-      if (!user) {
-        this.setState({ loading: false });
+    componentWillMount = async () => {
+      const index = document.cookie.indexOf("token");
+      if (index === -1) {
         this.props.history.push("/login");
       }
+      this.setState({ loading: false });
     };
     render = () => {
       return this.state.loading ? (
